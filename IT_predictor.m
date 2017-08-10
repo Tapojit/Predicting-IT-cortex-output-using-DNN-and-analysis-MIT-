581afb113147b69431125e4dbebbbfc9a75430cd
Tapojit <tapojitdebnath@gmail.com>
2017-08-10 14:41:03 -0400

    Update IT_predictor.m

--- IT_predictor.m
+++ IT_predictor.m
@@ -1,6 +1,12 @@
 function [ IT_matrix_pred ] = IT_predictor( model )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%Returns a file whose name consists of: model name followed by '_IT.mat'. Contains:
+%IT data matrix predicted using ridge regression model trained from specified model features. 
+
+%Input:
+%model=Keyword representing one of the 5 Neural/DNN model feature matrix used for ridge regression.
+%They are: 'HMO','Kr','Ze', 'V1', 'V2'
+
+%IT data path
 IT_data_path='PLoSCB2014_data_20141216/PLoSCB2014_data_20141216/NeuralData_IT_multiunits';
 IT_data=load(IT_data_path);
 IT_data_features=IT_data.features;
@@ -24,9 +30,11 @@ model_f_shuff=model_f(randperm(size(model_f,1)),:);
 IT_shuff=IT_data_features(randperm(size(IT_data_features,1)),:);
 
 IT_matrix_pred=zeros(size(IT_data_features));
+%Ridge regression for each channel of IT
 for m=1:size(IT_data_features,2)
     train_subsam_size=ceil(size(IT_shuff,1)/3);
     val_r_sq=zeros(1,10);
+    %Ten train-test splits for individual IT channels
     for a=1:10
         r_sq_val=zeros(1,3);
 
@@ -51,6 +59,7 @@ for m=1:size(IT_data_features,2)
                 val_sub_f=model_f_shuff(1:train_subsam_size,:);
                 val_sub_l=IT_shuff(1:train_subsam_size,m);
             end
+            %Training/creating encoding model
             b=ridge_r(training_sub_l,training_sub_f,'alpha', a);
 
             p_hat=mtimes(val_sub_f,b);
@@ -62,7 +71,7 @@ for m=1:size(IT_data_features,2)
 
     end
     [err_min,ind]=max(val_r_sq);
-
+    %Creating encoding model
     b2=ridge_r(IT_data_features(:,m),model_f,'alpha',ind);
     IT_matrix_pred(:,m)=mtimes(model_f,b2);
     disp(['lap ' num2str(m) ' of ' num2str(size(IT_data_features,2)) ' complete' ])
