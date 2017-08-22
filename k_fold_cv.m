55e370d78b959b3d201bc1bde5f5dfc280641162
Tapojit Debnath Tapu <tdebnath@gpu-16.cbmm.local>
2017-08-22 15:39:07 -0400

    Add existing file

--- k_fold_cv.m
+++ k_fold_cv.m
@@ -22,7 +22,7 @@ classdef k_fold_cv < handle
         %resp_vec=response vetor
         %hyp_arr=array of hyperparameters over which crossvalidation is carried out
         %folds=number of folds in crossvalidation
-        %reg_alg=String representing algorithm used for regression. Eg: 'Ridge_reg' represents ridge regression
+        %reg_alg=Object representing algorithm used for regression. Eg: 'reg_ridge' represents ridge regression
         %loss=String representing loss function. Eg: 'RMSE' represents Root mean squared error. 'MSE' represents Mean squared error.
         %%%%%Returns%%%%%%%%
         %object
@@ -77,23 +77,20 @@ classdef k_fold_cv < handle
                         cv_cov_val = cov_matrix((part_1_sum+1:part_2_sum),:);
                         cv_resp_val = resp_vec((part_1_sum+1:part_2_sum),:);
                     end
-                    %If ridge regression is chosen as preferred regression algorithm:
-                    if strcmp(obj.reg_alg, 'Ridge_reg')
-                        %Validation beta
-                        beta = ridge_reg(cv_resp_tr, cv_cov_tr, obj.hyp_arr(m));
-                        pred_vector = predict(beta, cv_cov_val);
+                    %Validation beta
+                    obj.reg_alg.fit(cv_resp_tr, cv_cov_tr, obj.hyp_arr(m));
+                    pred_vector = obj.reg_alg.predict(cv_cov_val);
 
-                    end
                     %%%Calculating validation loss%%%%
                     
                     %For RMSE loss:
                     if strcmp(obj.loss, 'RMSE')
 
-                        hyp_loss_arr(a) = rmse_loss(pred_vector, cv_resp_val);
+                        hyp_loss_arr(a) = obj.reg_alg.rmse_loss(pred_vector, cv_resp_val);
                     %For MSE loss:
                     else
 
-                        hyp_loss_arr(a) = mse_loss(pred_vector, cv_resp_val);
+                        hyp_loss_arr(a) = obj.reg_alg.mse_loss(pred_vector, cv_resp_val);
 
                     end
 
@@ -119,78 +116,12 @@ classdef k_fold_cv < handle
             
             obj.hyp_arr = [1:10];
             obj.folds = 3;
-            obj.reg_alg = 'Ridge_reg';
+            obj.reg_alg = reg_ridge;
             obj.loss = 'RMSE';
             
         end
         
-        %Carries out ridge regression and returns decision rule (beta)
-        %%%%%%%Arguments%%%%%
-        %obj=object
-        %resp_vector=response vector
-        %cov_matrix=covariate matrix
-        %alpha=hyperparameter value
-        %%%%%Returns%%%%%
         
-        %beta=decision rule
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function beta = ridge_reg(obj, resp_vector, cov_matrix, alpha )
-
-            x_f = size(cov_matrix, 2);
-
-            G = (alpha^2)*eye(x_f);
-
-            G(1,1) = 0;
-
-            beta = mtimes(inv(mtimes(cov_matrix.',cov_matrix)+G),mtimes(cov_matrix.',resp_vector));
-        
-        end
-        
-        %RMSE (Root Mean Squared Error) loss function
-        %%%%%%Arguments%%%%%%
-        %obj=object
-        %predicted=predicted response vector
-        %actual=actual response vector
-        
-        %%%%%%%Returns%%%%%%%%
-        %loss=RMSE loss value
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function loss = rmse_loss(obj, predicted, actual)
-            
-            loss = sqrt(mean((predicted-actual).^2));
-        
-        end
-        
-        %MSE (Mean Squared Error) loss function
-        %%%%%%Arguments%%%%%%
-        %obj=object
-        %predicted=predicted response vector
-        %actual=actual response vector
-        
-        %%%%%%%Returns%%%%%%%%
-        %loss=MSE loss value
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function loss = mse_loss(obj, predicted, actual)
-            
-            loss = mean((predicted-actual).^2);
-            
-        end
-        
-        %Makes predictions on test covariates. Returns predicted response
-        %vector.
-        %%%%%%Arguments%%%%%%
-        %obj=object
-        %beta=decision rule from training
-        %test_features=Test covariate matrix
-        
-        %%%%%%%Returns%%%%%%%%
-        %predicted_figures=predicted response vector
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function predicted_figures=predict(obj, beta, test_features)
-            
-            predicted_figures = mtimes(test_features, beta);
-
-        end
 
 
     end
---
+++ reg_ridge.m
@@ -0,0 +1,76 @@
+%Ridge regression object
+classdef reg_ridge < handle
+    properties
+        beta
+    end
+    
+    methods
+        
+        %Carries out ridge regression and returns decision rule (beta)
+        %%%%%%%Arguments%%%%%
+        %obj=object
+        %resp_vec=response vector
+        %cov_matrix=covariate matrix
+        %alpha=hyperparameter value
+        %%%%%Returns%%%%%
+        
+        %obj.beta=decision rule
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function obj = fit(cov_matrix, resp_vec, alpha)
+            x_f = size(cov_matrix, 2);
+
+            G = (alpha^2)*eye(x_f);
+
+            G(1,1) = 0;
+
+            obj.beta = mtimes(inv(mtimes(cov_matrix.',cov_matrix)+G),mtimes(cov_matrix.',resp_vec));
+
+        end
+        
+        
+        %Makes predictions on test covariates. Returns predicted response
+        %vector.
+        %%%%%%Arguments%%%%%%
+        %obj=object
+        %test_features=Test covariate matrix
+        
+        %%%%%%%Returns%%%%%%%%
+        %predicted_figures=predicted response vector
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function predicted_figures=predict(obj, test_features)
+            
+            predicted_figures = mtimes(test_features, obj.beta);
+
+        end
+        
+        %RMSE (Root Mean Squared Error) loss function
+        %%%%%%Arguments%%%%%%
+        %obj=object
+        %predicted=predicted response vector
+        %actual=actual response vector
+        
+        %%%%%%%Returns%%%%%%%%
+        %loss=RMSE loss value
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function loss = rmse_loss(obj, predicted, actual)
+            
+            loss = sqrt(mean((predicted-actual).^2));
+        
+        end
+        
+        %MSE (Mean Squared Error) loss function
+        %%%%%%Arguments%%%%%%
+        %obj=object
+        %predicted=predicted response vector
+        %actual=actual response vector
+        
+        %%%%%%%Returns%%%%%%%%
+        %loss=MSE loss value
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function loss = mse_loss(obj, predicted, actual)
+            
+            loss = mean((predicted-actual).^2);
+            
+        end
+    end
+end
